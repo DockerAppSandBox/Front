3309b4ed24c382d07dcf94c49636b720b0734891
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-01-23 16:57:50 +0100

    update the _app.tsx and create the landingpage who call the cards

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,23 @@ import '@mantine/core/styles.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
-import { theme } from "../theme";
+import { theme } from '../theme';
+import { AuthProvider } from '../contexts/AuthContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider theme={theme}>
-      <Head>
-        <title>Mantine Template</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-        <link rel="shortcut icon" href="/favicon.svg" />
-      </Head>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <AuthProvider>
+      <MantineProvider theme={theme}>
+        <Head>
+          <title>Mantine Template</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+          />
+          <link rel="shortcut icon" href="/favicon.svg" />
+        </Head>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </AuthProvider>
   );
 }
