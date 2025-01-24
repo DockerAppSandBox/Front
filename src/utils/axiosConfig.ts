575035c6f6306f91b633ddbfa1c8e773a817989d
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-01-24 15:59:55 +0100

    update the authcontext for redirect user if not loging and configure axios

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
-
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
